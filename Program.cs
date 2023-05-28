4c1c96c63442969116ea30bf88cf50983253c0a5
Ej Sadiarin <ejsadiarin@gmail.com>
2023-05-28 22:27:29 +0800

    Happy Dahyun Day and Happy Anniversary of me Programming! I'm back doing C# again

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Heavenly World");
+Console.WriteLine(20 / 12);
+Console.WriteLine("Happy Dahyun Day and Happy Anniversary of me Programming");
+Console.WriteLine("Officially one year of programming!");
+Console.WriteLine("Hey I'm back home C#");
