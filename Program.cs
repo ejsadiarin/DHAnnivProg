54643e87693ab227c7289cc8a00f28c42da77c39
Ej Sadiarin <ejsadiarin@gmail.com>
2023-05-29 23:36:48 +0800

    ideavimrc config poggers

--- Program.cs
+++ Program.cs
@@ -5,3 +5,15 @@ Console.WriteLine(20 / 12);
 Console.WriteLine("Happy Dahyun Day and Happy Anniversary of me Programming");
 Console.WriteLine("Officially one year of programming!");
 Console.WriteLine("Hey I'm back home C#");
+
+static string Foo()
+{
+    int numberTest = 123;
+    string result = numberTest.ToString();
+
+    string bar = "hey";
+
+    return result;
+}
+
+Console.WriteLine(Foo());
